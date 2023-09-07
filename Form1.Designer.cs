d79b10b3c300492152798b646504515cb5dd4950
EdwardJhosep <1454668@senati.pe>
2023-09-07 11:20:49 -0500

    creamos el titulo-label, insertamos cntrol GROUPBOX

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Snap ITC", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(310, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(148, 42);
+            label1.TabIndex = 0;
+            label1.Text = "LOGIN";
+            label1.Click += label1_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Font = new Font("Stencil", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            groupBox1.Location = new Point(176, 77);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(442, 271);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "COMPLETE TODOS LOS CAMPOS";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private GroupBox groupBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,15 @@ namespace login
         {
             InitializeComponent();
         }
+        //INSERTAMOS UN LABEL PARA TITULO
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        //control GroupBox
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
     }
 }
